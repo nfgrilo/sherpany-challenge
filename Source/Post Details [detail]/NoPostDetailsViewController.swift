2724e48b6c9ac39bccc722c587168389becbf72e
Nuno Grilo <nfgrilo@gmail.com>
2018-08-17 11:06:40 +0100

    Added noPost/Post distinct states

---
+++ Source/Post Details [detail]/NoPostDetailsViewController.swift
@@ -0,0 +1,13 @@
+//
+//  NoPostDetailsViewController.swift
+//  Sherpany Posts
+//
+//  Created by Nuno Grilo on 17/08/2018.
+//  Copyright © 2018 NunoGrilo.com. All rights reserved.
+//
+
+import UIKit
+
+class NoPostDetailsViewController: UIViewController, Storyboarded {
+    
+}
--- Source/Post Details [detail]/PostDetailsCoordinator.swift
+++ Source/Post Details [detail]/PostDetailsCoordinator.swift
@@ -15,16 +15,61 @@ class PostDetailsCoordinator: Coordinator {
     /// The navigation view controller currently being used to present view controllers.
     var navigationController: UINavigationController
     
-    /// Weak reference to presented view controller.
-    ///
-    /// A strong reference is already made when vc is presented (added to vc hierarchy).
-    private weak var viewController: PostDetailsViewController?
+    /// Post details view controller (strong ref: will be in and out of view hierarchy).
+    private var postDetailsViewController: PostDetailsViewController?
+    
+    /// Empty view controller (strong ref: will be in and out of view hierarchy).
+    private var noPostDetailsViewController: NoPostDetailsViewController?
     
     /// Model controller.
     private let modelController: ModelController
     
     /// Posts data source.
     private var dataSource: PostDetailsDataSource?
+    
+    /// Post details coordinator states.
+    ///
+    /// - noSelection: No post selected.
+    /// - selected: Some post selected.
+    enum State {
+        case noSelection
+        case selected(Post)
+    }
+    
+    /// Post details coordinator state.
+    private var state: State = .noSelection {
+        didSet {
+            switch state {
+                
+            /// no post selected
+            case .noSelection:
+                guard let viewController = noPostDetailsViewController else {
+                    break
+                }
+                // -> present "no post" VC
+                DispatchQueue.main.async { [weak self] in
+                    if self?.navigationController.topViewController != viewController {
+                        self?.navigationController.viewControllers = [viewController]
+                    }
+                }
+                
+            // some post selected
+            case .selected(let post):
+                guard let viewController = postDetailsViewController else {
+                    break
+                }
+                // -> present post details VC
+                dataSource?.post = post
+                DispatchQueue.main.async { [weak self] in
+                    if self?.navigationController.topViewController != viewController {
+                        self?.navigationController.viewControllers = [viewController]
+                    }
+                    self?.postDetailsViewController?.tableView.reloadData()
+                }
+            }
+            
+        }
+    }
     
     
     /// Creates a coordinator.
@@ -37,20 +82,26 @@ class PostDetailsCoordinator: Coordinator {
     
     /// Take control!
     func start() {
-        // create & setup vc
-        guard let viewController = PostDetailsViewController.instantiate() else { return }
-        self.viewController = viewController
-        viewController.coordinator = self
+        // post details VC
+        guard let postDetailsViewController = PostDetailsViewController.instantiate() else { return }
+        self.postDetailsViewController = postDetailsViewController
+        postDetailsViewController.coordinator = self
+        postDetailsViewController.title = "Challenge Accepted!" // Requirement #1: ✅
+        
+        // no-post details VC
+        guard let noPostDetailsViewController = NoPostDetailsViewController.instantiate() else { return }
+        self.noPostDetailsViewController = noPostDetailsViewController
+        noPostDetailsViewController.title = "Challenge Accepted!" // Requirement #1: ✅
         
         // table view data source (post details)
         let dataSource = PostDetailsDataSource()
         self.dataSource = dataSource
-        viewController.tableView.dataSource = dataSource
+        postDetailsViewController.tableView.dataSource = dataSource
         
         // present it
-        navigationController.pushViewController(viewController, animated: false)
-        navigationController.topViewController?.title = "Challenge Accepted!" // Requirement #1: ✅
+        navigationController.pushViewController(noPostDetailsViewController, animated: false)
     }
+    
 }
 
 
@@ -60,23 +111,19 @@ extension PostDetailsCoordinator: PostSelectedDelegate {
     
     // Requirement #9: ✅
     func postSelected(postId: Int64?) {
-        // selection cleared out?
+        // no post selected?
         guard let postId = postId else {
-            dataSource?.post = nil
-            refreshUI()
+            state = .noSelection
             return
         }
 
         // get post from Core Data
-        modelController.post(with: postId, completion: { [weak self] post in
-            self?.dataSource?.post = post
-            self?.refreshUI()
-        })
-    }
-    
-    private func refreshUI() {
-        DispatchQueue.main.async { [weak self] in
-            self?.viewController?.tableView.reloadData()
+        modelController.post(with: postId) { [weak self] post in
+            guard let post = post else {
+                self?.state = .noSelection
+                return
+            }
+            self?.state = .selected(post)
         }
     }
     
